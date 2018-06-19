9355a9f19f207b78c55d71691d53a9ac99c3663e
Hanrui-Chen <37696281+Hanrui-Chen@users.noreply.github.com>
2018-06-19 01:47:25 +0000

    Update flaskr.py

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-import mapcalc as mapcalc
+import mapcalc
 
 app = Flask(__name__)
 app.config.from_object(__name__)
